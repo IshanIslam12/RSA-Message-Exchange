eeaee20511c18ecc9d8dd86682ef53fe6f95789a
IshanIslam12 <121452644+IshanIslam12@users.noreply.github.com>
2024-05-05 21:38:51 -0400

    RSA-Message-Exchange.py

--- RSA-Message-Exchange.py
+++ RSA-Message-Exchange.py
@@ -2,20 +2,6 @@ from ai265 import *
 
 #insert sender's public key in "person"
 person = RsaPublicKey.read_from("""
------BEGIN RSAISH PUBLIC KEY-----
-Bits: 2044
-Owner: Chris League
-Generated: 2024-04-28 23:22:21+00:00
-
-AwABAAEAAUOuE/S0fEKARV7zJqLiAyneqcYixvyr3aUJvTF7
-dbAw2Hy0RSbDyH8dBSNh8J55PJqiElFJmGWm4Ld45yBjbU8W
-cdsvd7hXHw2UGX6BYhkNsNeOZa/ALnwM1KXhFP7D6p+oR8by
-0aWHITJUhaXXUPZboa/5H+kBEkBwsTHvEdQmoPZ+57+Vbs+3
-ge5RHLiicXszg8kIa7L6r/A5gipQ5cRHYNrZjLVuFgGJiGdL
-Y1OcT/cGY6vsZhSR2jiiUHxknMC9n2WFNFFshk0w/X2rLOyE
-Vmp/X4PXBlQV9yXq5jFklPutFNyYu7GwmYkQPzxLGcTjzVSi
-ehb599gg8DP/rwk=
------END RSAISH PUBLIC KEY-----
 
 """)
 
